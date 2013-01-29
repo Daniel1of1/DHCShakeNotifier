dbe724dd7129601b93148b933fabb016f5cfd1b6
Confidence <confidence.designed@gmail.com>
2013-01-29 15:13:27 +0000

    remove nsnotification observer

--- DHCShakeNotifierDemo/ViewController.m
+++ DHCShakeNotifierDemo/ViewController.m
@@ -49,4 +49,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:DHCSHakeNotifName object:nil];
+}
+
 @end
