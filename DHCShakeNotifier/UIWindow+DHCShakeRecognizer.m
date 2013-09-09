99e71cd9436edf2951e6bb5fe47a5706ab2d64bb
Daniel Haight <confidence.designed@gmail.com>
2013-09-09 17:42:49 +0100

    add modal blurry stuff

--- DHCShakeNotifier/UIWindow+DHCShakeRecognizer.m
+++ DHCShakeNotifier/UIWindow+DHCShakeRecognizer.m
@@ -7,13 +7,27 @@
 //
 
 #import "UIWindow+DHCShakeRecognizer.h"
+#import <objc/runtime.h>
 
 @implementation UIWindow (DHCShakeRecognizer)
 
-- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
++(void)load {
+    Method original = class_getInstanceMethod([self class], @selector(motionEnded:withEvent:));
+    Method dhcPrefixed = class_getInstanceMethod([self class],@selector(dhc_motionEnded:withEvent:));
+    
+    method_exchangeImplementations(original, dhcPrefixed);
+}
+
+- (void)dhc_motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
     if (event.type == UIEventTypeMotion && event.subtype == UIEventSubtypeMotionShake) {
         [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:@"CONJUShakeNotification" object:nil]];
     }
+    if ([UIWindow instancesRespondToSelector:@selector(dhc_motionEnded:withEvent:)]) {
+        [self dhc_motionEnded:motion withEvent:event];
+    }
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
 }
 
 @end
